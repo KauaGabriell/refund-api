733a649643b50be842fe7d5719ad50ce7226772e
KauaGabriell <kauagabriell64@gmail.com>
2026-05-25 09:37:20 -0300

    feat: implement refund creation logic in RefundController

--- src/controllers/refundController.ts
+++ src/controllers/refundController.ts
@@ -1,10 +1,38 @@
 import type { Request, Response } from "express";
 import { z } from "zod";
+import { prisma } from "../libs/prisma.js";
 import { AppError } from "../utils/AppError.js";
 
 class RefundController {
   async create(request: Request, response: Response) {
-    return response.status(200).json({ message: "ok" });
+    const bodySchema = z.object({
+      name: z
+        .string()
+        .min(2, { message: "The name must have at least 2 characters." })
+        .trim(),
+      amount: z.number().positive(),
+      category: z.enum([
+        "food",
+        "others",
+        "services",
+        "transport",
+        "accommodation",
+      ]),
+      filename: z.string(),
+    });
+    if (!request.user) throw new AppError("Unauthorized", 401);
+    const { name, amount, category, filename } = bodySchema.parse(request.body);
+    const refund = await prisma.refunds.create({
+      data: {
+        name,
+        amount,
+        category,
+        filename,
+        userId: request.user.id,
+      },
+    });
+
+    return response.status(200).json(refund);
   }
 }
 
